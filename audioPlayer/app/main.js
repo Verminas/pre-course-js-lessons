35df4132c5675f2619273be9ee65ab65add04e90
Verminas <veremeeva96@gmail.com>
2024-02-29 18:13:26 +0300

    feat: add files

--- audioPlayer/app/main.js
+++ audioPlayer/app/main.js
@@ -1,8 +1,48 @@
 // data
 const playlist = {
-    title: 'Rammstein',
-    coverImage: './assets/img/',
-    audioSrc:  './assets/audio/',
+    playListId: '1',
+    playListInfo: {
+        title: 'Hip-Hop hits',
+        coverImgUrl: 'assets/img/hip-hop-cover.webp',
+        totalInfo: {
+            totalTrackCount: 4,
+            totalTrackDurationInSec: 733,
+        }
+    },
+    tracks: [
+        {
+            trackTitle: 'Rap God',
+            artistName: 'Eminem',
+            trackCoverImageUrl: 'assets/img/rap_god.jpg',
+            trackFileUrl: 'assets/audio/rap_god.mp3',
+            trackDurationInSec: 333,
+            trackId: '10',
+        },
+        {
+            trackTitle: 'In da club',
+            artistName: '50cent',
+            trackCoverImageUrl: 'assets/img/in_da_club.jpg',
+            trackFileUrl: 'assets/audio/in_da_club.mp3',
+            trackDurationInSec: 333,
+            trackId: '11',
+        },
+        {
+            trackTitle: 'Crew',
+            artistName: 'GoldLink',
+            trackCoverImageUrl: 'assets/img/crew.jpeg',
+            trackFileUrl: 'assets/audio/in_da_club.mp3',
+            trackDurationInSec: 333,
+            trackId: '11',
+        },
+        {
+            trackTitle: 'All eyes on me ',
+            artistName: '2pac',
+            trackCoverImageUrl: 'assets/img/all_eyes.png',
+            trackFileUrl: 'assets/audio/rap_god.mp3',
+            trackDurationInSec: 333,
+            trackId: '11',
+        },
+    ],
 }
 
 // render
