fe680ada8d24e11c19d062617f241830f567377a
Verminas <veremeeva96@gmail.com>
2024-02-29 17:37:18 +0300

    feat: add structure for project

---
+++ audioPlayer/app/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const wrapperPage = document.createElement('div');
+wrapperPage.classList.add('wrapper_page');
+document.body.appendChild(wrapperPage);
+
+const wrapperPlayer = document.createElement('div');
+wrapperPlayer.classList.add('wrapper_player');
+wrapperPage.appendChild(wrapperPlayer);
+
